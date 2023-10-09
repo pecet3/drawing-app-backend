39eca5e91bd74b33413c5835ee332067578fd578
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-09 20:49:39 +0200

    .

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var socket_io_1 = require("socket.io");
+var io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*"
+    },
+});
+var connections = 0;
+io.on("connection", function (socket) {
+    console.log("\uD83D\uDC9A Connection numer: ".concat(connections));
+    connections++;
+    socket.on("client-ready", function () {
+        socket.broadcast.emit("get-canvas-state");
+    });
+    var canvasState;
+    socket.on("canvas-state", function (state) {
+        console.log("💛 canvas state server");
+        canvasState = state;
+        console.log(canvasState.slice(0, 24));
+        socket.broadcast.emit("canvas-state-from-server", canvasState);
+    });
+    socket.on("draw-line", function (_a) {
+        var prevPoint = _a.prevPoint, currentPoint = _a.currentPoint, color = _a.color, size = _a.size;
+        socket.broadcast.emit("draw-line", { prevPoint: prevPoint, currentPoint: currentPoint, color: color, size: size });
+    });
+    socket.on("clear", function () { return io.emit("clear"); });
+});
+server.listen(3001, function () {
+    console.log("💚 Listening on port 3001 💚");
+});
