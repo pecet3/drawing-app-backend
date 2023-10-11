56e9355b53d4c23e9efdab09fd9a8bd387414a3f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-12 01:13:13 +0200

    .

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var cors = require('cors');
 var socket_io_1 = require("socket.io");
 var app = express();
+app.use(cors({
+    origin: "*"
+}));
 var server = http.createServer(app);
 var io = new socket_io_1.Server(server, {
     cors: {
@@ -25,8 +29,9 @@ io.on("connection", function (socket) {
         console.log(canvasState.slice(0, 24));
         socket.broadcast.emit("canvas-state-from-server", canvasState);
     });
-
-
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
     socket.on("draw-line", function (_a) {
         var prevPoint = _a.prevPoint, currentPoint = _a.currentPoint, color = _a.color, size = _a.size;
         socket.broadcast.emit("draw-line", { prevPoint: prevPoint, currentPoint: currentPoint, color: color, size: size });
@@ -34,14 +39,11 @@ io.on("connection", function (socket) {
     socket.on("clear", function () { return io.emit("clear"); });
 });
 
-
-
-const port = 80
+const port = 3000
 server.listen(port, function () {
     console.log(`💚 Listening on port ${port} 💚`);
-
     app.get('/', function (req, res) {
-        console.log("User visited")
+        console.log("User visited");
         res.json({ message: "hello world" });
     });
-}); 
+});
